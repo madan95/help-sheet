e35eb8b94691237a8d8f6b8db935fc05fb124b0b
madan95 <madan_limbu@hotmail.co.uk>
2019-06-19 23:26:47 +0100

    Format

--- src/component/element-viewport/index.js
+++ src/component/element-viewport/index.js
@@ -7,47 +7,47 @@ export default class ElementViewPort extends React.PureComponent {
   }
 
   componentDidMount() {
-    console.log('Component did mount.');
-    const el = document.querySelector('.floating__box');
+    console.log("Component did mount.");
+    const el = document.querySelector(".floating__box");
     this.checkEl(el);
-    window.addEventListener('resize', () => this.checkEl(el), false);
-    window.addEventListener('scroll', () => this.checkEl(el), false);
+    window.addEventListener("resize", () => this.checkEl(el), false);
+    window.addEventListener("scroll", () => this.checkEl(el), false);
   }
 
-  checkEl = (el) => {
-      const float = document.querySelector('.floating__float');
-      console.log(this.isElementInViewport(el));
-      if (!this.isElementInViewport(el)) {
-        float.classList.remove('hidden');
-      }
-      else {
-          float.classList.add('hidden');
-      }
-  }
-
-  isElementInViewport  = (el) => {
-        let rect = el.getBoundingClientRect();
-
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
+  checkEl = el => {
+    const float = document.querySelector(".floating__float");
+    console.log(this.isElementInViewport(el));
+    if (!this.isElementInViewport(el)) {
+      float.classList.remove("hidden");
+    } else {
+      float.classList.add("hidden");
     }
+  };
+
+  isElementInViewport = el => {
+    let rect = el.getBoundingClientRect();
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
 
   render() {
     return (
-        <>
+      <>
         <div className="floating__box">
-            (BOX-1)
-            Scroll or resize till this box partially is hidden.
+          (BOX-1) Scroll or resize till this box partially is hidden.
         </div>
-      <div className="floating__float">
+        <div className="floating__float">
           <div>
-          Hello, this is floating element (BOX-2). Only visible when box (BOX-1) is outside the viewport.
+            Hello, this is floating element (BOX-2). Only visible when box
+            (BOX-1) is outside the viewport.
           </div>
-      </div>
+        </div>
       </>
     );
   }
